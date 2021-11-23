d76f6f71f6ffcbe28f20365a285ef59f8bcadfb0
viivek <59990645+viivekxio@users.noreply.github.com>
2021-11-23 12:24:02 +0530

    cartbtn

--- js/main.js
+++ js/main.js
@@ -237,7 +237,22 @@ $(document).ready(function() {
     }
 }
               
-                      
+//COPY COUPON
+
+function copyText(element) {
+  var $copyText = document.getElementById(element).innerText;
+  var button = document.getElementById(element + '-button');
+  navigator.clipboard.writeText($copyText).then(function() {
+    var originalText = button.innerText;
+    button.innerText = 'Copied!';
+    setTimeout(function(){
+      button.innerText = originalText;
+      }, 750);
+  }, function() {
+    button.style.cssText = "background-color: var(--red);";
+    button.innerText = 'Error';
+  });
+}
 
 
 
